c38e553c9b1c35815f11890007eef0ef3708cf4c
Amit Gupta <your@email.com>
2025-09-21 02:37:09 +0530

    rock pattern

--- app7.java
+++ app7.java
@@ -1,6 +1,6 @@
 public class app7 {
     public static void main(String[] args) {
-        for(int i=1; i<5; i++){
+        for(int i=1; i<=5; i++){
             for(int j=5; j>=i; j--){
                 System.out.print(" ");
             }

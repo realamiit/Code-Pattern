3d0e65d89d85be4345bdcf3563c227dd565f3b48
Amit Gupta <your@email.com>
2025-09-20 18:12:29 +0530

    commit

--- app3.java
+++ app3.java
@@ -1,20 +1,20 @@
 public class app3 {
     public static void main(String[] args) {
-        int n=5;
-        int m=6;
+        int n=4;
+        int m=5;
         for(int i=1; i<=n; i++){
             for(int j=1; j<=m; j++){
                 
                 if(i==1 || j==1 || n==i || m==j) {
-                    System.out.println("*");
+                    System.out.print("*");
                 }else{
-                    System.out.print("");
+                    System.out.print(" ");
                 }
 
-                System.out.println("");
-                }
             }
+               System.out.println("");
         }
-
     }
+
+}
 

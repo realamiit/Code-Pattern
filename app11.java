f8c53c84938c829007c087d069de668404edde89
Amit Gupta <your@email.com>
2025-09-21 00:44:44 +0530

    next pattern

--- app11.java
+++ app11.java
@@ -1,16 +1,15 @@
 public class app11 {
-    public static void main (String args[]){
-        for(int i=1; i<=5; i++){
-            for(int j=1; j<=i; j++){
-                int num = i+j;
-                if(num % 2 == 0){
+    public static void main(String args[]) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                int num = i + j;
+                if (num % 2 == 0) {
                     System.out.print("1 ");
-                }else{
+                } else {
                     System.out.print("0 ");
                 }
             }
             System.out.println();
         }
-
     }
 }

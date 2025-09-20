d854c91ed7d1aa07a3a8590ca6ee19c4a951287d
Amit Gupta <your@email.com>
2025-09-20 17:32:32 +0530

    go with java3

---
+++ app3.java
@@ -0,0 +1,20 @@
+public class app3 {
+    public static void main(String[] args) {
+        int n=5;
+        int m=6;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                
+                if(i==1 || j==1 || n==i || m==j) {
+                    System.out.println("*");
+                }else{
+                    System.out.print("");
+                }
+
+                System.out.println("");
+                }
+            }
+        }
+
+    }
+

1c8b089e764475fd9cbd0363d346e90a4249926c
Amit Gupta <your@email.com>
2025-10-05 00:09:58 +0530

    update something

--- app13.java
+++ app13.java
@@ -1,5 +1,4 @@
 public class app13 {
-
     public static void main(String[] args) {
         int n=5;
        for(int i=1; i<=n; i++){
@@ -12,3 +11,5 @@ public class app13 {
        }
     }
 }
+
+
---
+++ app14.java
@@ -0,0 +1,12 @@
+public class app14 {
+    public static void main(String[] args) {
+        int n=3;
+        int m=15;
+        int sum=n+m;
+        System.out.println("Sum is: "+sum);
+        System.out.println("Difference is: "+(n-m));
+        System.out.println("Product is: "+(n*m));
+        System.out.println("Quotient is: "+(n/m));
+    }
+    
+}
